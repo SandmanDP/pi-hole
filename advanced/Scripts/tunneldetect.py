46e72c9f80e6770bd7aa64957de5e41657c53ade
Isaac Wahhab <fwahhab@apple.com>
2020-11-25 15:24:34 -0800

    Logic for domain processing

--- advanced/Scripts/tunneldetect.py
+++ advanced/Scripts/tunneldetect.py
@@ -1,4 +1,40 @@
+from socket import AF_INET, SOCK_STREAM, socket
 import sys
 
 
-sys.stderr.write('google.com blah.net')
+with socket(AF_INET, SOCK_STREAM) as s:
+    s.connect(('localhost', 4711))
+    s.send(b'>getallqueries\n')
+    data = b''
+    while True:
+        data += s.recv(4096)
+        if data[-12:] == b'\n---EOM---\n\n':
+            break
+# with open('/Users/fwahhab/Downloads/dns_test_data.txt', 'rb') as f:
+#     data = f.read()
+
+
+domains = {}
+
+
+def process_domain(domain_elements, timestamp, parent_elements, parent_dict):
+    domain, *subelements = domain_elements
+
+    if domain not in parent_dict:
+        parent_dict[domain] = {'count': 0, 'timestamps': [], 'subelements': {}}
+
+    domain_dict = parent_dict[domain]
+    domain_dict['count'] += 1
+    domain_dict['timestamps'].append(timestamp)
+
+    if subelements:
+        process_domain(subelements, timestamp, [*parent_elements, domain], domain_dict['subelements'])
+
+
+for line in data[:-12].decode("utf-8").split('\n'):
+    timestamp_str, _, addr_str, *_ = line.split()
+    addr_elements = addr_str.split('.')
+    process_domain(['.'.join(addr_elements[-2:]), *reversed(addr_elements[:-2])], int(timestamp_str), [], domains)
+
+
+sys.stderr.write(' '.join(domains.keys()))
